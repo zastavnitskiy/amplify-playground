af80cd961e0f6ccb8ba5c94cf2539143617e3145
Pavel Zastavnitskiy <pasha.zastavnitskiy@gmail.com>
2019-09-24 22:15:40 +0200

    Create and Delete works

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React from 'react';
-import './App.css';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+import * as mutations from "./graphql/mutations";
+import { listOrders } from "./graphql/queries";
 Amplify.configure(awsconfig);
-
-const listTodos = `query listTodos {
-  listTodos{
-    items{
-      id
-      name
-      description
-    }
-  }
-}`
-
-const addTodo = `mutation createTodo($name:String! $description: String!) {
-  createTodo(input:{
-    name:$name
-    description:$description
-  }){
-    id
-    name
-    description
-  }
-}`
-
-const createOrder = `mutation createOrder($name:String! $description: String!) {
-  createOrder(input:{
-    name:$name
-    description:$description
-  }){
-    id
-    name
-    description
-  }
-}`
-
-
 
 // next:
 
@@ -47,29 +14,82 @@ const createOrder = `mutation createOrder($name:String! $description: String!) {
 // delete order mutation
 // view all orders
 function App() {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [lastModifiedOrderId, setLastModifiedOrderId] = useState(null);
 
-  const todoMutation = async () => {
-    const todoDetails = {
-      name: 'Party tonight!',
-      description: 'Amplify CLI rocks!'
-    };
-    
-    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
-    alert(JSON.stringify(newTodo));
-  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    API.graphql(
+      graphqlOperation(mutations.createOrder, {
+        input: {
+          name,
+          url
+        }
+      })
+    ).then(result => {
+      setLastModifiedOrderId(result.data.createOrder.id);
+    });
+  };
 
-  const listQuery = async () => {
-    console.log('listing todos');
-    const allTodos = await API.graphql(graphqlOperation(listTodos));
-    alert(JSON.stringify(allTodos));
-  }
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    let isActive = true;
+    API.graphql(
+      graphqlOperation(listOrders, {
+        limit: 20
+      })
+    ).then(result => {
+      console.log("got orders", result.data.listOrders.items);
+      if (isActive) {
+        setOrders(result.data.listOrders.items);
+      }
+    });
+
+    return () => (isActive = false);
+  }, [lastModifiedOrderId]);
+
+  const handleDelete = event => {
+    event.preventDefault();
+    API.graphql(
+      graphqlOperation(mutations.deleteOrder, {
+        input: {
+          id: event.target.dataset.id
+        }
+      })
+    ).then(result => {
+      console.log("order created", result.data.deleteOrder.id);
+      setLastModifiedOrderId(result.data.deleteOrder.id);
+    });
+  };
 
   return (
     <div className="App">
-        <p> Pick a file</p>
-        <button onClick={listQuery}>GraphQL Query</button>
-        <button onClick={todoMutation}>GraphQL Mutation</button>
-      </div>
+      <ul>
+        {orders.map(order => {
+          return (
+            <li key={order.id}>
+              <span>{order.name}</span>
+              <span>{order.url}</span>
+              <span>{order.id}</span>
+              <button onClick={handleDelete} data-id={order.id}>
+                ×
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input name="url" value={url} onChange={e => setUrl(e.target.value)} />
+        <button type="submit">Create</button>
+      </form>
+    </div>
   );
 }
 
